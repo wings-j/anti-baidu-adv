ae08a00b1b4b4f462e908a74446140b7201c9f2e
jiangyi <jiangyi@bertadata.com>
2021-12-29 13:26:00 +0800

    update

--- tool/build.js
+++ tool/build.js
@@ -2,41 +2,28 @@
  * 构建
  */
 
-const { exec } = require('child_process')
-const { promises: Fs } = require('fs')
-const Path = require('path')
-const Package = require('../package.json')
+import { exec } from 'child_process'
+import { promises as Fs } from 'fs'
 
-/**
- * 输出
- */
-async function emit() {
-  return new Promise(resolve => {
-    exec('tsc', (er, stdout, stderror) => {
-      if (er) {
-        console.error(er)
-        console.log(stdout)
+const packageJson = JSON.parse(await Fs.readFile('./package.json'))
 
-        reject(er)
-      } else {
-        resolve()
-      }
-    })
+await new Promise(resolve => {
+  exec('rollup -c', (er, stdout, stderror) => {
+    if (er) {
+      console.error(er)
+      console.log(stdout)
+      console.log(stderror)
+
+      reject(er)
+    } else {
+      resolve()
+    }
   })
-}
+})
 
-/**
- * 制作Tampermonkey
- */
-async function makeTampermonkey() {
-  let [source, template] = await Promise.all([
-    Fs.readFile(Path.resolve(__dirname, '../dist/index.js'), 'utf-8'),
-    Fs.readFile(Path.resolve(__dirname, '../src/shell/tampermonkey/template.js'), 'utf-8')
-  ])
+let source = await Fs.readFile('./dist/index.js', 'utf-8')
 
-  let target = template.replace('/* ${code} */', source).replace('${version}', Package.version)
-  await Fs.mkdir(Path.resolve(__dirname, '../dist/tampermonkey'), { recursive: true })
-  await Fs.writeFile(Path.resolve(__dirname, '../dist/tampermonkey/index.js'), target)
-}
-
-emit().then(makeTampermonkey)
+let tampermonkeyTemplate = await Fs.readFile('./src/shell/tampermonkey/template.js', 'utf-8')
+let tampermonkeyTarget = tampermonkeyTemplate.replace('/* ${code} */', source).replace('${version}', packageJson.version)
+await Fs.mkdir('./dist/tampermonkey', { recursive: true })
+await Fs.writeFile('./dist/tampermonkey/index.js', tampermonkeyTarget)
